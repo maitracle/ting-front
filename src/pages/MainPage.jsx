ae7fff859e9ca04d717bf82eb2cb85d32cf408ec
maitracle <maitracle@gmail.com>
2020-02-07 15:43:42 +0900

    REF: trailing black line 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Counter from '../components/StoreComponent';
 
-export default () => <div>
-  <Counter />
-</div>
+export default () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
