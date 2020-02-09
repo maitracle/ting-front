7468c5895a87dd25bb9510d77b46751674c95145
maitracle <maitracle@gmail.com>
2020-02-10 04:08:30 +0900

    CHG: 환경에 따라 api host 변경하게 수정

--- src/constants/requests.js
+++ src/constants/requests.js
@@ -1,4 +1,8 @@
-export const HOST = 'http://localhost:8000';
+const connectionEnvToHost = {
+  development: 'http://localhost:8000',
+};
+
+export const HOST = connectionEnvToHost[process.env.NODE_ENV];
 export const LOGIN_PATH = '/api/tokens/';
 export const REFRESH_TOKENS_PATH = '/api/tokens/refresh/';
 export const FETCH_LIKE_PATH = '/api/likes/';
