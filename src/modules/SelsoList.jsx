01f59655fc533d7265a8bd6dce8748c5c30ec57a
outwater <kocon135@naver.com>
2020-02-17 16:41:23 +0900

    err: 리스트 출력 오류

--- src/modules/SelsoList.jsx
+++ src/modules/SelsoList.jsx
@@ -1,21 +1,37 @@
 import React, { useState, useEffect } from "react";
+import { toJS } from "mobx";
 import { inject, observer } from "mobx-react";
 
-const SelsoList = ({ setSelsoList, selsoList, test }) => {
+const SelsoList = ({ setSelsoList, selsoList, test, getSelsoList }) => {
   useEffect(() => {
     setSelsoList();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
+  // const a = JSON.stringify(selsoList);
+  // const b = JSON.parse(a);
+  // const c = JSON.parse(JSON.stringify(selsoList));
+  useEffect(() => {
+    // console.log(JSON.parse(JSON.stringify(selsoList))[0]);
+    // console.log(toJS(selsoList, { exportMapsAsObjects: true }));
+    console.log("get", typeof getSelsoList()[0]);
+  }, [getSelsoList, selsoList]);
+  const tojs = toJS(selsoList);
   return (
     <div>
-      selsolist:{selsoList}
-      {test}
+      <p>selsolist:</p>
+      {/* {tojs} */}
+      {/* {tojslist} */}
+      {/* {selsoList} */}
+      {/* {selsoList}
+      {selsoList}
+      {test} */}
     </div>
   );
 };
+
 export default inject(({ selsoListStore }) => ({
   setSelsoList: selsoListStore.setSelsoList,
   selsoList: selsoListStore.selsoList,
-  test: selsoListStore.test
+  test: selsoListStore.test,
+  getSelsoList: selsoListStore.getSelsoList
 }))(observer(SelsoList));
--- src/stores/SelsoListStore.js
+++ src/stores/SelsoListStore.js
@@ -1,25 +1,35 @@
-import { action, observable } from "mobx";
+import { action, observable, toJS } from "mobx";
 
 import requests from "src/utils/requests";
 import { GET_PROFILE_LISTS_PATH } from "src/constants/requests";
 
 export default class SelsoListStore {
-  @observable selsoList = [123];
+  @observable selsoList = [];
 
   constructor(root) {
     this.root = root;
   }
-  @action setSelsoList = () => {
-    console.log(localStorage.access);
-    const a = localStorage.getItem("access");
-    console.log(a);
 
+  @action getSelsoList = () => {
+    return this.selsoList;
+  };
+
+  @action setSelsoList = () => {
     requests
-      .get(GET_PROFILE_LISTS_PATH, true) //jwt token어떻게 태워서 보낼것인가?
+      .get(GET_PROFILE_LISTS_PATH, true)
       .then(res => {
-        console.log(res);
-        this.selsoList = [];
+        // console.log(res.data[0].id);
         this.selsoList = res.data;
+        // // this.selsoList = res.data;
+        // this.selsoList.push(res.data);
+        // // console.log(typeof res.data);
+        // // console.log(res.data[1]);
+        // // console.log(this.selsoList[1].id);
+        // // console.log(typeof this.selsoList);
+        // // console.dir(this.selsoList);
+        // console.log(this.selsoList[0][0]);
+        // console.log(this.selsoList[0][0]["id"]);
+        // console.log(this.selsoList[0][0].id);
       })
       .catch(err => {
         return err;
