ad642c05595d17bf71c4740256845327e0ca834e
maitracle <maitracle@gmail.com>
2020-02-19 14:15:30 +0900

    CHG: eslint rule 추가, rule에 따라 코드 수정

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -4,40 +4,33 @@ import { HOST } from 'src/constants/requests';
 import { fetchAccessTokenWithRefreshToken, getAccessTokenOrRedirect } from 'src/utils/handleJwtToken';
 
 
-const getRequestConfig = (isNeededAuth) => {
-  return {
-    headers: {
-      Authorization: isNeededAuth ? `Bearer ${getAccessTokenOrRedirect()}` : '',
-    }
-  };
-};
+const getRequestConfig = (isNeededAuth) => ({
+  headers: {
+    Authorization: isNeededAuth ? `Bearer ${getAccessTokenOrRedirect()}` : '',
+  },
+});
 
 const handleUnAuthorizedError = (err, path, isNeededAuth) => {
   if (isNeededAuth && err.response.status === 401) {
     return fetchAccessTokenWithRefreshToken()
       .then(() => {
         if (path.slice(-1) !== '#') {
-          return requests.get(path + '#', isNeededAuth)
-            .then((res) => {
-              return res;
-            })
+          // eslint-disable-next-line no-use-before-define
+          return requests.get(`${path}#`, isNeededAuth)
+            .then((res) => res);
         }
-        else {
-          throw 'prevent too many request';
-        }
-      })
-  } else {
-    throw err;
+
+        // Todo(maitracle): 임시로 disable 한 lint rule을 해결한다.
+        // eslint-disable-next-line no-throw-literal
+        throw 'prevent too many request';
+      });
   }
+  throw err;
 };
 
 const requests = {
-  get: (path, isNeededAuth = false) => {
-    return axios.get(HOST + path , getRequestConfig(isNeededAuth))
-      .catch((err) => {
-        return handleUnAuthorizedError(err, path, isNeededAuth);
-      })
-  },
+  get: (path, isNeededAuth = false) => axios.get(HOST + path, getRequestConfig(isNeededAuth))
+    .catch((err) => handleUnAuthorizedError(err, path, isNeededAuth)),
   post: (path, data, isNeededAuth = false) => axios.post(HOST + path, data, getRequestConfig(isNeededAuth))
     .catch((err) => handleUnAuthorizedError(err, path, isNeededAuth)),
   put: (path, data, isNeededAuth) => axios.put(HOST + path, data, getRequestConfig(isNeededAuth))
