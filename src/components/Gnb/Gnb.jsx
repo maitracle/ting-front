4ed11bc1de053b1902422bb2314c8e38be38c106
outwater <kocon135@naver.com>
2020-02-21 14:54:00 +0900

    fix: single quote으로 변경

--- src/components/Gnb/Gnb.jsx
+++ src/components/Gnb/Gnb.jsx
@@ -1,19 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import "./Gnb.scss";
+import './Gnb.scss';
 
-export const Gnb = () => {
-  return (
-    <>
-      <div className="title">
+export const Gnb = () => (
+  <>
+    <div className="title">
         GNB
-        <div className="title-sub">sub title</div>
-      </div>
-      <Link to="/">/</Link>&nbsp;&nbsp;&nbsp;
-      <Link to="/another">/another</Link>&nbsp;&nbsp;&nbsp;
-      <Link to="/login">/login</Link>&nbsp;&nbsp;&nbsp;
-      <Link to="/selsolist">Selsolist</Link>&nbsp;&nbsp;&nbsp;
-    </>
-  );
-};
+      <div className="title-sub">sub title</div>
+    </div>
+    <Link to="/">/</Link>
+&nbsp;&nbsp;&nbsp;
+    <Link to="/another">/another</Link>
+&nbsp;&nbsp;&nbsp;
+    <Link to="/login">/login</Link>
+&nbsp;&nbsp;&nbsp;
+    <Link to="/selsolist">Selsolist</Link>
+&nbsp;&nbsp;&nbsp;
+  </>
+);
