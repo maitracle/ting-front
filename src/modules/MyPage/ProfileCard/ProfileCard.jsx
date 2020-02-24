8e8dbe357a6be98e77455f177644005d37f84dc5
maitracle <maitracle@gmail.com>
2020-02-24 23:41:41 +0900

    CHG: image 없는경우 틀 무너지는 문제 수정

--- src/modules/MyPage/ProfileCard/ProfileCard.jsx
+++ src/modules/MyPage/ProfileCard/ProfileCard.jsx
@@ -4,7 +4,9 @@ import './ProfileCard.scss';
 
 export const ProfileCard = ({ imageSrc, nickname }) => (
   <div>
-    <img src={imageSrc} className={'image'} alt={'프로필 이미지'} />
+    <div className={'image'}>
+      <img src={imageSrc} className={'image'} alt={'프로필 이미지'} />
+    </div>
     <div className={'nicknameWrapper'}><span className={'nickName'}>{ nickname }</span></div>
     <div className={'aperture'} />
   </div>
