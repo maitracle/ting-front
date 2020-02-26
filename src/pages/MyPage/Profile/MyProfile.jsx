233624bed012dd1e9dbacd6d432dbb0a00a4ace9
maitracle <maitracle@gmail.com>
2020-02-26 17:05:14 +0900

    CHG: className을 큰따옴표로 사용

--- src/pages/MyPage/Profile/MyProfile.jsx
+++ src/pages/MyPage/Profile/MyProfile.jsx
@@ -18,53 +18,53 @@ export const MyProfile = inject('userStore')(observer(({ userStore }) => {
   const updateProfile = () => userStore.updateProfile(profileData);
 
   return (
-    profileData ?
+    userStore.isLoggedIn ?
       <div>
         <ProfileCard imageSrc={userStore.profile.image} nickname={userStore.profile.nickname} />
 
-        <section className={'sectionWrapper'}>
-          <div className={'inputWrapper'}>
-            <div className={'label'}>오픈카카오 링크</div>
-            <input value={profileData.chatLink || ''} onChange={setForm('chatLink')} className={'inputBox'} type={'text'}/>
+        <section className="sectionWrapper">
+          <div className="inputWrapper">
+            <div className="label">오픈카카오 링크</div>
+            <input value={profileData.chatLink || ''} onChange={setForm('chatLink')} className="inputBox" type="text" />
           </div>
 
-          <div className={'inputWrapper'}>
-            <div className={'label'}>태그</div>
-            <input value={profileData.tags || ''} onChange={setForm('tags')} className={'inputBox'} type={'text'}/>
+          <div className="inputWrapper">
+            <div className="label">태그</div>
+            <input value={profileData.tags || ''} onChange={setForm('tags')} className="inputBox" type="text" />
           </div>
 
-          <div className={'inputWrapper'}>
-            <div className={'label'}>한줄 자기소개</div>
-            <input value={profileData.oneSentence || ''} onChange={setForm('oneSentence')} className={'inputBox'} type={'text'}/>
+          <div className="inputWrapper">
+            <div className="label">한줄 자기소개</div>
+            <input value={profileData.oneSentence || ''} onChange={setForm('oneSentence')} className="inputBox" type="text"/>
           </div>
         </section>
 
-        <div className={'separator'} />
+        <div className="separator" />
 
-        <section className={'sectionWrapper'}>
-          <div className={'textBoxWrapper'}>
-            <div className={'label'}>외모 / 스타일</div>
-            <textarea value={profileData.appearance || ''} onChange={setForm('appearance')} className={'textArea'} />
+        <section className="sectionWrapper">
+          <div className="textBoxWrapper">
+            <div className="label">외모 / 스타일</div>
+            <textarea value={profileData.appearance || ''} onChange={setForm('appearance')} className="textArea" />
           </div>
 
-          <div className={'textBoxWrapper'}>
-            <div className={'label'}>성격</div>
-            <textarea value={profileData.personality || ''} onChange={setForm('personality')} className={'textArea'} />
+          <div className="textBoxWrapper">
+            <div className="label">성격</div>
+            <textarea value={profileData.personality || ''} onChange={setForm('personality')} className="textArea" />
           </div>
 
-          <div className={'textBoxWrapper'}>
-            <div className={'label'}>취미 / 여가</div>
-            <textarea value={profileData.hobby || ''} onChange={setForm('hobby')} className={'textArea'} />
+          <div className="textBoxWrapper">
+            <div className="label">취미 / 여가</div>
+            <textarea value={profileData.hobby || ''} onChange={setForm('hobby')} className="textArea" />
           </div>
 
-          <div className={'textBoxWrapper'}>
-            <div className={'label'}>연애관</div>
-            <textarea value={profileData.dateStyle || ''} onChange={setForm('dateStyle')} className={'textArea'} />
+          <div className="textBoxWrapper">
+            <div className="label">연애관</div>
+            <textarea value={profileData.dateStyle || ''} onChange={setForm('dateStyle')} className="textArea" />
           </div>
 
-          <div className={'textBoxWrapper'}>
-            <div className={'label'}>이상형</div>
-            <textarea value={profileData.idealType || ''} onChange={setForm('idealType')} className={'textArea'} />
+          <div className="textBoxWrapper">
+            <div className="label">이상형</div>
+            <textarea value={profileData.idealType || ''} onChange={setForm('idealType')} className="textArea" />
           </div>
         </section>
         <button style={{border: 'solid 1px black'}} onClick={updateProfile}>update</button>
