b136bf64541bdf73dcaae7591fbf8b5c019906b3
maitracle <39800081+maitracle@users.noreply.github.com>
2020-02-27 15:18:54 +0900

    Feat/my page connect data (#18)
    * CHG: my list page 와 store data 연결
    
    * CHG: my profile page 와 store data 연결
    
    * ADD: my profile update 기능을 추가한다.
    
    * CHG: className을 큰따옴표로 사용

--- src/constants/requests.js
+++ src/constants/requests.js
@@ -5,8 +5,11 @@ const HostDictionaryByEnv = {
 
 export const HOST = HostDictionaryByEnv[process.env.NODE_ENV];
 export const GET_PROFILE_LISTS_PATH = '/api/profiles/';
-export const LOGIN_PATH = '/api/tokens/';
+export const LOGIN_PATH = '/api/users/tokens/';
+export const REFRESH_TOKENS_PATH = '/api/users/tokens/refresh/';
 export const GET_MY_PROFILE_PATH = '/api/profiles/my/';
 export const SIGN_IN_PATH = '/api/users/';
-export const REFRESH_TOKENS_PATH = '/api/tokens/refresh/';
+
 export const FETCH_LIKE_PATH = '/api/likes/';
+
+export const UPDATE_PROFILE_PATH = '/api/profiles/';
--- src/pages/MyPage/List/MyList.jsx
+++ src/pages/MyPage/List/MyList.jsx
@@ -4,37 +4,38 @@ import { Link } from 'react-router-dom';
 import { ProfileCard } from 'src/modules/MyPage/ProfileCard/ProfileCard';
 
 import './MyList.scss';
+import { inject, observer } from 'mobx-react';
 
 
-export const MyList = () => {
+export const MyList = inject('userStore')(observer(({ userStore }) => {
 
   return (
     <div>
-      <ProfileCard imageSrc={'./blahblah.png'} nickname={'날강두'} />
+      <ProfileCard imageSrc={userStore.profile.image} nickname={userStore.profile.nickname} />
 
-      <div className={'linkWrapper'}>
-        <Link to={'/my/profile'}>
-          <div className={'linkItem'}>
-            <span className={'linkText'}>마이 프로필 수정하기</span>
+      <div className="linkWrapper">
+        <Link to="/my/profile">
+          <div className="linkItem">
+            <span className="linkText">마이 프로필 수정하기</span>
           </div>
         </Link>
-        <Link to={'/my/diamond'}>
-          <div className={'linkItem'}>
-            <span className={'linkText'}>다이아 변동사항</span>
+        <Link to="/my/diamond">
+          <div className="linkItem">
+            <span className="linkText">다이아 변동사항</span>
           </div>
         </Link>
-        <Link to={'/my/questions'}>
-          <div className={'linkItem'}>
-            <span className={'linkText'}>자주 묻는 질문</span>
+        <Link to="/my/questions">
+          <div className="linkItem">
+            <span className="linkText">자주 묻는 질문</span>
           </div>
         </Link>
-        <Link to={'/my/review'}>
-          <div className={'linkItem'}>
-            <span className={'linkText'}>리뷰 남기기</span>
+        <Link to="/my/review">
+          <div className="linkItem">
+            <span className="linkText">리뷰 남기기</span>
           </div>
         </Link>
 
       </div>
     </div>
   );
-};
+}));
