27339d2d6da99b406dfaa09731e40dd798dcfdf1
10000001a <pgdn06288888@gmail.com>
2020-02-28 17:23:47 +0900

    merge master 3

--- src/routers/Routes.jsx
+++ src/routers/Routes.jsx
@@ -2,13 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import DefaultLayoutRouter from './DefaultLayoutRouter';
-<<<<<<< HEAD
 import UserConfirmPage from 'src/pages/UserConfirmPage';
 import SignUpRouter from 'src/routers/SignUpRouter';
-=======
-import SignUpRouter from 'src/routers/SignUpRouter';
-import UserConfirmPage from '../pages/UserConfirmPage';
->>>>>>> 4f4237545d1698da4c05e3751123e79fe426bc91
 
 
 export default () => (
