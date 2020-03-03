a13e9a8e8b0ce2d7233ae6e4f63f87d7c8f49898
maitracle <39800081+maitracle@users.noreply.github.com>
2020-03-03 10:29:58 +0900

    CHG: gnb style 수정 (#24)

--- src/components/Gnb/Gnb.jsx
+++ src/components/Gnb/Gnb.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './Gnb.scss';
+import styles from  './Gnb.module.scss';
 
 export const Gnb = () => (
-  <>
-    <div className="title">
-        GNB
-      <div className="title-sub">sub title</div>
+  <div className={styles.gnbWrapper}>
+    <div className={styles.gnbContents}>
+      <Link to={'/selso'}><img src={require('src/assets/images/Logo/Camcou.png')} className={styles.logoImage} alt={'캠쿠'} /></Link>
+      <div className={styles.pointWrapper}>
+        <div className={styles.point}>포인트 <span className={styles.count}>{'10'}</span></div>
+      </div>
     </div>
-    <Link to="/">/</Link>{'   '}
-    <Link to="/login">/login</Link>{'   '}
-    <Link to="/selso">Selso</Link>{'   '}
-  </>
+  </div>
 );
