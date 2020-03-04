82b57972c231a5da32c5988aa2ce9d3d0fb5fd33
outwater <kocon135@naver.com>
2020-03-04 21:51:09 +0900

    console.log 제거

--- src/modules/Register/StepComponents/Group3/OneSentence.jsx
+++ src/modules/Register/StepComponents/Group3/OneSentence.jsx
@@ -4,7 +4,6 @@ import Textarea from 'src/components/Input/Textarea';
 
 const OneSentence = inject('registerStore')(
   observer(({ registerStore }) => {
-    console.log(registerStore.registerData);
     const setOneSentence = (e) => registerStore.setRegisterData('oneSentence', e.target.value);
     const placeholder = '35자 이내로 자신에 대해 표현해주세요 :)';
     return (
