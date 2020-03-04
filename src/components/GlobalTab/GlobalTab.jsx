8966b6d3c1615a73686682ce0dab604e0d106706
maitracle <39800081+maitracle@users.noreply.github.com>
2020-03-04 18:16:14 +0900

    style: tab component 스타일 수정 (#28)

--- src/components/GlobalTab/GlobalTab.jsx
+++ src/components/GlobalTab/GlobalTab.jsx
@@ -1,23 +1,46 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
-import './GlobalTab.scss';
+import styles from './GlobalTab.module.scss';
 
 
-const TabItem = withRouter(({ name, linkTo }) => (
-  <Link to={linkTo} className={'tabLink'}>
-    <div className={'tabItem ' + (location.pathname.startsWith(linkTo) ? ' selected' : '')}>
-      {name}
-    </div>
-  </Link>
-));
+const TabItem = withRouter(({ name, linkTo, imageSrc, selectedImageSrc }) => {
+  const isSelected = location.pathname.startsWith(linkTo);
+  return (
+    <Link to={linkTo} className={styles.tabLink}>
+      <div className={styles.tabItem}>
+        <img
+          src={isSelected ? selectedImageSrc : imageSrc}
+          className={styles.image}
+          alt={name}
+        />
+        <div className={isSelected ? styles.labelSelected : ''}>{name}</div>
+      </div>
+    </Link>
+  );
+});
 
 export const GlobalTab = () => {
   return (
-    <div className={'tabWrapper'}>
-      <TabItem name={'List'} linkTo={'/selso'} />
-      <TabItem name={'ZZim'} linkTo={'/zzim'} />
-      <TabItem name={'MyPage'} linkTo={'/my'} />
+    <div className={styles.tabWrapper}>
+      <TabItem
+        name={'List'}
+        linkTo={'/selso'}
+        imageSrc={require('src/assets/images/GlobalTab/home.png')}
+        selectedImageSrc={require('src/assets/images/GlobalTab/home-selected.png')}
+      />
+      <TabItem
+        name={'ZZim'}
+        linkTo={'/zzim'}
+        imageSrc={require('src/assets/images/GlobalTab/heart.png')}
+        selectedImageSrc={require('src/assets/images/GlobalTab/heart-selected.png')}
+      />
+      <TabItem
+        name={'MyPage'}
+        linkTo={'/my'}
+        imageSrc={require('src/assets/images/GlobalTab/person.png')}
+        selectedImageSrc={require('src/assets/images/GlobalTab/person-selected.png')}
+      />
     </div>
   );
 };
