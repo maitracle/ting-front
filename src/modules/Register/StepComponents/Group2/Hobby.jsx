7a256705f9981a5557ad47196744dcaeda8e5ae5
outwater <kocon135@naver.com>
2020-03-04 21:54:05 +0900

    ADD: Textarea컴포넌트 생성, 스텝2 컴포넌트 수정 (#31)
    * Textarea컴포넌트 생성, 스텝3 생성
    
    * console.log 제거

--- src/modules/Register/StepComponents/Group2/Hobby.jsx
+++ src/modules/Register/StepComponents/Group2/Hobby.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
-
+import Textarea from 'src/components/Input/Textarea';
 
 const Hobby = inject('registerStore')(
   observer(({ registerStore }) => {
-    const setOneSentence = (e) => registerStore.setOneSentence(e.target.value);
+    const setHobby = (e) => registerStore.setRegisterData('hobby', e.target.value);
+    const placeholder = '60자 이상, 많이 많이 쓸수록 이성분이 매력적으로 생각할거에요 :)';
     return (
-      <div className="survey">
-        <div className="anwserWrap">
-          <p> HobbyHobbyHobbyHobby한줄표현입력하세요</p>
-          <input type="text" value={registerStore.registerData.oneline} onChange={setOneSentence} />
-        </div>
+      <div>
+        <Textarea placeholder={placeholder} value={registerStore.registerData.hobby} onChange={setHobby} />
       </div>
     );
   }),
--- src/stores/RegisterStore/RegisterStore.js
+++ src/stores/RegisterStore/RegisterStore.js
@@ -23,6 +23,12 @@ export class RegisterStore {
     bodyType: '',
     religion: '',
     isSmoke: '',
+    apperance: '',
+    personality: '',
+    hobby: '',
+    dateStyle: '',
+    idealType: '',
+    oneSentence: '',
   };
 
   constructor(root) {
