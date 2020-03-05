74a41360d59a28d1cef8421e3e37a887be4740d3
maitracle <maitracle@gmail.com>
2020-03-06 05:41:36 +0900

    STYLE: register header

--- src/modules/Register/TabBar.jsx
+++ src/modules/Register/TabBar.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 import styles from './TabBar.module.scss';
 
 
+const mapStepToStep = {
+  Age: '나이',
+  Height: '키',
+  BodyType: '체형',
+  Religion: '종교',
+  IsSmoke: '흡연여부',
+  Appearance: '외모/스타일',
+  Personality: '성격',
+  Hobby: '취미/여가',
+  DateStyle: '연애관',
+  IdealType: '이상형',
+  OneSentence: '한줄을',
+  Tags: '간단한 테그',
+  Image: '사진',
+  ChatLink: '',
+};
+
 const TabBar = ({ currentStep, tabList }) => {
 
   const getRate = () => `${((tabList.indexOf(currentStep) + 1) / tabList.length) * 100}%`;
@@ -9,7 +26,7 @@ const TabBar = ({ currentStep, tabList }) => {
   return (
     <div className={styles.tabBarWrapper}>
       <div className={styles.tabItemWrapper}>
-        {tabList.map((item) => <div className={styles.tabItem} key={item}>{item}</div>)}
+        {tabList.map((item) => <div className={`${styles.tabItem}`} key={item}>{ mapStepToStep[item] }</div>)}
       </div>
       <div className={styles.tabProgressBar}>
         <div style={{ width: getRate() }} className={styles.tabProgressBarRate} />
