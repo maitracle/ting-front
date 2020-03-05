6b6a893a0de33ff09f74f6f2926535ea782595f1
maitracle <39800081+maitracle@users.noreply.github.com>
2020-03-06 04:25:11 +0900

    Feat/get link button (#37)
    * ADD: selso/detail 페이지에 button 추가
    
    * ADD: fetch open kakao lint action 추가

--- src/routers/Routes.jsx
+++ src/routers/Routes.jsx
@@ -6,11 +6,13 @@ import SignUpRouter from 'src/routers/SignUpRouter';
 import RegisterRouter from 'src/routers/RegisterRouter';
 import DefaultLayoutRouter from './DefaultLayoutRouter';
 import LogInPage from 'src/pages/LogInPage';
+import SelsoDetailPage from 'src/pages/SelsoDetailPage';
 
 export default () => (
   <Router>
     <main>
       <Switch>
+        <Route path="/selso/detail" exact component={SelsoDetailPage} />
         <Route path="/log-in" exact component={LogInPage} />
         <Route path="/sign-up" exact component={SignUpRouter} />
         <Route path="/user-confirm/:userCode" exact component={UserConfirmPage} />
