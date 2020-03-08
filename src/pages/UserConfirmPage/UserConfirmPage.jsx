80b0c99bbd2b1d1bc4e1fa46d97a31d3afa919e8
10000001a <pgdn06288888@gmail.com>
2020-03-08 18:39:35 +0900

    ADD: UserConfirmpage css 추가

--- src/pages/UserConfirmPage/UserConfirmPage.jsx
+++ src/pages/UserConfirmPage/UserConfirmPage.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react'
 import { inject, observer } from 'mobx-react';
-import { useParams, Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+
+import styles from './UserConfirmPage.module.scss';
 
 
 
-const UserConfirmPage = inject('signUpStore')(observer(({ signUpStore }) => {
+const UserConfirmPage = inject('signUpStore')(observer(({ signUpStore, history }) => {
 
   const { userCode } = useParams();
 
@@ -14,25 +16,33 @@ const UserConfirmPage = inject('signUpStore')(observer(({ signUpStore }) => {
 
   const title = ( nickname ) => {
     return(
-      <div>
-        <p>홍익대학교 학생</p>
-        <p>{ nickname }님 인증 완료</p>
+      <div className={styles.titleWrapper}>
+        <p className={styles.title}>
+          홍익대학교 학생<br/>
+          { nickname }님 인증 완료
+        </p>
       </div>
     )}
+
+  const linkToSelso = () => history.push('/selso')
+
+  const linkToRegister = () => history.push('/register')
   
-  // Todo(10000001a): a 태그와 button에 링크 아직 부여 안함.
+  // Todo(10000001a): div를 img로 변경.
   return (
-    <div>
+    <div className={styles.pageWrapper}>
       { title(signUpStore.root.userStore.profile.nickname) }
-      <p>4단계를 통해</p>
-      <p>캠쿠와 함께 마이프로필을 완성해봐요!</p>
-      <div>이미지</div>
-      <Link to={'/selso'}>
-        <div>skip</div>
-      </Link>
-      <Link>
-        <button>만들러 가기</button>
-      </Link>
+      <div>
+        <p className={styles.description}>
+          4단계를 통해<br/>
+          캠쿠와 함께 마이프로필을 완성해봐요!
+        </p>
+      </div>
+      <div className={styles.image}></div>
+      <div className={styles.skipButton} onClick={linkToSelso}>
+        skip
+      </div>
+      <button className={styles.nextButton}onClick={linkToRegister}>만들러 가기</button>
     </div>
   );
 }));
