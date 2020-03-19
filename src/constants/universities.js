593b9707321c61e3006992390069d5e7bf9d1598
maitracle <maitracle@gmail.com>
2020-03-19 20:07:38 +0900

    ADD: university, campus 상수 추가

---
+++ src/constants/universities.js
@@ -0,0 +1,8 @@
+export const HONGIK = 'HONGIK';
+export const HONGIK_CAMPUSES = ['SEOUL'];
+
+export const YONSEI = 'YONSEI';
+export const YONSEI_CAMPUSES = ['SINCHON', 'INTERNATIONAL'];
+
+export const KYUNGHEE = 'KYUNGHEE';
+export const KYUNGHEE_CAMPUSES = ['SEOUL', 'INTERNATIONAL'];
