1ac53ccda8aa72e84290ae46ec4536c80152e9cc
maitracle <maitracle@gmail.com>
2020-03-23 00:23:11 +0900

    change: image input label 추가, 미리보기 추가

--- src/modules/SignUp/CheckStudentIdCard.jsx
+++ src/modules/SignUp/CheckStudentIdCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { inject, observer } from 'mobx-react';
 import { withRouter } from 'react-router-dom';
 
@@ -10,6 +10,7 @@ import styles from './CheckStudentIdCard.module.scss';
 
 const CheckStudentIdCard = inject('signUpStore')(observer(({ signUpStore, history }) => {
   const [idCardImage, setIdCardImage] = useState(null);
+  const [imageSrc, setImageSrc] = useState(null);
   const [errorMessage, setErrorMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
@@ -17,6 +18,18 @@ const CheckStudentIdCard = inject('signUpStore')(observer(({ signUpStore, histor
     setErrorMessage('');
     setIdCardImage(e.target.files[0]);
   };
+
+  useEffect(() => {
+    if (idCardImage) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setImageSrc(e.target.result);
+      };
+
+      reader.readAsDataURL(idCardImage);
+    }
+  }, [idCardImage]);
+
 
   const submit = () => {
     if (!idCardImage) {
@@ -41,7 +54,16 @@ const CheckStudentIdCard = inject('signUpStore')(observer(({ signUpStore, histor
     <>
       <div className={styles.formWrapper}>
         <div className={styles.inputWrapper}>
-          <input type="file" onChange={uploadIdCardImage} accept=".jpg,.png" />
+          <input className={styles.imageInput} id="id-card" type="file" onChange={uploadIdCardImage} accept=".jpg,.png" />
+          <label htmlFor="id-card">
+            <div className={styles.imageLabel}>
+              { imageSrc ?
+                <img className={styles.imagePreview} src={imageSrc} alt="학생증 이미지 미리보기" />
+                :
+                <div className={styles.labelDescription}>이미지를 업로드해주세요.</div>
+              }
+            </div>
+          </label>
           <div className={styles.errorMessage}>{errorMessage}</div>
         </div>
 
