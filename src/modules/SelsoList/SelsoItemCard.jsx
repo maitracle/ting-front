68f1e793d54d73f9684b4a6aa792a059f817da50
outwater <kocon135@naver.com>
2020-04-06 22:11:23 +0900

    FEAT/filter SelfdateList by gender & university (#65)
    * 중복import제거
    
    * list apiCall 수정
    
    * filter list by gender,university
    
    * list의 age계산해서 출력
    
    * profile PATH 실수 수정
    
    * age를 현재Date로 수정

--- src/modules/SelsoList/SelsoItemCard.jsx
+++ src/modules/SelsoList/SelsoItemCard.jsx
@@ -4,14 +4,19 @@ import styles from './SelsoItemCard.module.scss';
 
 
 export default function SelsoItemCard({ selsoItem }) {
+  const today = new Date();
+  const age = today.getFullYear() - selsoItem.profile.bornYear + 1;
+
   return (
     <div className={styles.card}>
       <div className={styles.leftBox}>
         <div className={styles.titleBox}>
-          <div className={styles.nickname}>{selsoItem.nickname} </div>
+          <div className={styles.nickname}>
+            {selsoItem.nickname}
+          </div>
           <div className={styles.age}>
-            {`${selsoItem.age}살`}
-            <img src={require('src/assets/images/SelsoList/New@3x.png')} className={styles.new} alt='new' />
+            {`${age}살`}
+            <img src={require('src/assets/images/SelsoList/New@3x.png')} className={styles.new} alt="new" />
           </div>
         </div>
         <div className={styles.tags}>{selsoItem.tags}</div>
@@ -20,7 +25,7 @@ export default function SelsoItemCard({ selsoItem }) {
         </div>
       </div>
       <div className={styles.rightBox}>
-        <img src={selsoItem.image || require('src/assets/images/defaultProfileImage.jpg')} className={styles.image} alt={'프로필 이미지'} />
+        <img src={selsoItem.image || require('src/assets/images/defaultProfileImage.jpg')} className={styles.image} alt="프로필 이미지" />
       </div>
     </div>
   );
