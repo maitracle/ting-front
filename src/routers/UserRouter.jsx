b5a79a9b0e18be63fe1feebf1e5ca8665a07403c
10000001a <pgdn06288888@gmail.com>
2020-04-09 00:25:50 +0900

    CNG: Change useEffect trigger.

--- src/routers/UserRouter.jsx
+++ src/routers/UserRouter.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
 
@@ -7,22 +7,17 @@ import RegisterCompletePage from 'src/pages/Selso/RegisterCompletePage';
 import LogInPage from 'src/pages/User/LogInPage';
 import EmailConfirmPage from 'src/pages/User/UserConfirmPage';
 import SignUpPage from 'src/pages/User/SignUpPage';
-import SignUpStore from '../stores/SignUpStore';
 
 
 export default inject('userStore')(observer(({ userStore }) => {
-  useEffect(()=>{}, [SignUpStore.step, userStore.user, userStore.profile])
+  useEffect(()=>{}, [userStore.user])
 
   const component = () => userStore.user.isConfirmedStudent === true ? <Redirect to="/user/register"/> : <SignUpPage />
 
   return (
     <Switch>
       <Route path="/user/log-in" exact component={LogInPage} />
-      <Route path="/user/sign-up/:university" exact  component={component}
-        // render = {
-        //   ()=>(setStep() === true ? (<Redirect to='/user/register' />) : (<SignUpPage />))
-        // }
-      />
+      <Route path="/user/sign-up/:university" exact  component={component} />
       <Route path="/user/email-confirm/:userCode" exact component={EmailConfirmPage} />
       <Route path="/user/register" exact component={RegisterPage} />
       <Route path="/user/register/complete" exact component={RegisterCompletePage} />
