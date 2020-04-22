4a2dbbd78547ca8cbd8f311eb40c7a954cd41d68
maitracle <maitracle@gmail.com>
2020-04-22 19:24:21 +0900

    delete: router에 필요없는 페이지 제거

--- src/routers/DefaultLayoutRouter/DefaultLayoutRouter.jsx
+++ src/routers/DefaultLayoutRouter/DefaultLayoutRouter.jsx
@@ -6,8 +6,6 @@ import SelsoListPage from 'src/pages/Selso/SelsoListPage';
 import MyList from 'src/pages/MyPage/List';
 import MyProfile from 'src/pages/MyPage/Profile';
 import MyPoint from 'src/pages/MyPage/Point';
-import MyQuestion from 'src/pages/MyPage/Question';
-import MyReview from 'src/pages/MyPage/Review';
 import FroshListPage from 'src/pages/Frosh/FroshListPage';
 
 
@@ -23,8 +21,6 @@ export default () => {
       <Route path="/my" exact component={MyList} />
       <Route path="/my/profile" exact component={MyProfile} />
       <Route path="/my/point" exact component={MyPoint} />
-      <Route path="/my/question" exact component={MyQuestion} />
-      <Route path="/my/review" exact component={MyReview} />
     </>
   );
 };
