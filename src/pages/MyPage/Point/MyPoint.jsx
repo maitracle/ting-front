b559405bbfe2497c1872588147be31866a6c2f7c
maitracle <maitracle@gmail.com>
2020-04-22 21:03:52 +0900

    fix: coin history page에 key가 잘못되어 있던 문제 수정

--- src/pages/MyPage/Point/MyPoint.jsx
+++ src/pages/MyPage/Point/MyPoint.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
 import MyPointHistory from 'src/pages/MyPage/Point/MyPointHistory';
+
 import styles from './MyPoint.module.scss';
+
 
 export const MyPoint = inject('myPointStore')(observer(({ myPointStore }) => {
   return (
@@ -17,7 +19,10 @@ export const MyPoint = inject('myPointStore')(observer(({ myPointStore }) => {
           {myPointStore.restPoint} Pts
         </div>
       </div>
-      {myPointStore.myPointHistoryList.map((item) => <MyPointHistory key={item.id} item={item}/>)}
+      <div className={styles.aperture} />
+      <div>
+        {myPointStore.myPointHistoryList.map((item) => <MyPointHistory key={item.id} item={item} />)}
+      </div>
     </div>
   );
   
