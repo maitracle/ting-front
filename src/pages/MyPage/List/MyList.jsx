82251050d48e01f5f406d91e7f5da52ad15cdcc6
maitracle <maitracle@gmail.com>
2020-04-22 19:15:49 +0900

    delete: 필요없는 기능 제거

--- src/pages/MyPage/List/MyList.jsx
+++ src/pages/MyPage/List/MyList.jsx
@@ -24,21 +24,10 @@ export const MyList = inject('userStore')(observer(({ userStore }) => {
             <span className={styles.linkText}>포인트 변동사항</span>
           </div>
         </Link>
-        <Link to="/my/questions">
-          <div className={styles.linkItem}>
-            <span className={styles.linkText}>자주 묻는 질문</span>
-          </div>
-        </Link>
-        <Link to="/my/review">
-          <div className={styles.linkItem}>
-            <span className={styles.linkText}>리뷰 남기기</span>
-          </div>
-        </Link>
 
         <div onClick={() => userStore.logOut()} className={styles.linkItem}>
           <span className={styles.linkText}>로그아웃</span>
         </div>
-
       </div>
     </div>
   );
