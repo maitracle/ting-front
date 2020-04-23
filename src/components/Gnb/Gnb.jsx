69eefd30609c70d62938f51f856d546a09f7f429
maitracle <39800081+maitracle@users.noreply.github.com>
2020-04-23 16:49:11 +0900

    change: SideBar component 이름 Panel로 변경, panel list style 수정 (#77)

--- src/components/Gnb/Gnb.jsx
+++ src/components/Gnb/Gnb.jsx
@@ -3,51 +3,72 @@ import { Link } from 'react-router-dom';
 
 import { inject, observer } from 'mobx-react';
 import { FiMenu } from 'react-icons/fi';
-import SideBar from 'src/components/SideBar';
+import { Panel } from './Panel';
+
 import styles from './Gnb.module.scss';
 
 
-export const Gnb = inject('myPointStore')(observer(({ myPointStore }) => {
-  const [isOpenSideBarModal, setisOpenSideBarModal] = useState(false);
+export const Gnb = inject('myPointStore', 'userStore')(observer(({ myPointStore, userStore }) => {
+  const [isOpenPanel, setIsOpenPanel] = useState(false);
 
   return (
     <>
       <div className={styles.gnbWrapper}>
         <div className={styles.gnbContents}>
-          <Link to="/selso"><img src={require('src/assets/images/Logo/Camcou.png')} className={styles.logoImage} alt="캠쿠" /></Link>
+          <Link to="/">
+            <img src={require('src/assets/images/Logo/Camcou.png')} className={styles.logoImage} alt="캠쿠" />
+          </Link>
           <div className={styles.pointWrapper}>
             <div className={styles.point}>
               포인트
-              <span className={styles.count}>{ myPointStore.restPoint }</span>
+              <span className={styles.count}>{myPointStore.restPoint}</span>
             </div>
           </div>
-          <div className={styles.sideBarWrapper}>
-            <FiMenu onClick={() => setisOpenSideBarModal(true)} />
-            <SideBar
-              isOpen={isOpenSideBarModal}
-              close={() => setisOpenSideBarModal(false)}
+          <div className={styles.panelWrapper}>
+            <FiMenu onClick={() => setIsOpenPanel(true)} />
+            <Panel
+              isOpen={isOpenPanel}
+              close={() => setIsOpenPanel(false)}
             >
-              <Link to="/selso">
-                <div className={styles.link} onClick={() => setisOpenSideBarModal(false)}>
-                  List보기
-                </div>
-              </Link>
-              <Link to="/my">
-                <div className={styles.link} onClick={() => setisOpenSideBarModal(false)}>
-                  Mypage
-                </div>
-              </Link>
               <Link to="/">
-                <div className={styles.link} onClick={() => setisOpenSideBarModal(false)}>
-                  공지사항
+                <img src={require('src/assets/images/Logo/Camcou.png')} className={styles.logoImage} alt="캠쿠" />
+              </Link>
+
+              <hr className={styles.separator} />
+
+              <div className={styles.serviceTitle}>
+                셀프 소개팅
+              </div>
+              <Link to="/selso" className={styles.link}>
+                <div className={styles.linkTitle} onClick={() => setIsOpenPanel(false)}>
+                  목록 보기
                 </div>
               </Link>
-              <Link to="/my/question">
-                <div className={styles.link} onClick={() => setisOpenSideBarModal(false)}>
-                  문의사항
+              <Link to="/user/register" className={styles.link}>
+                <div className={styles.linkTitle} onClick={() => setIsOpenPanel(false)}>
+                  프로필 등록하기
                 </div>
               </Link>
-            </SideBar>
+
+              <hr className={styles.separator} />
+
+              {
+                userStore.isLoggedIn ?
+                  <Link to="/my" className={styles.link}>
+                    <div className={styles.linkTitle} onClick={() => setIsOpenPanel(false)}>
+                      마이페이지
+                    </div>
+                  </Link>
+                  :
+                  <>
+                    <Link to="/user/log-in" className={styles.link}>
+                      <div className={styles.linkTitle} onClick={() => setIsOpenPanel(false)}>
+                        로그인
+                      </div>
+                    </Link>
+                  </>
+              }
+            </Panel>
           </div>
         </div>
       </div>
