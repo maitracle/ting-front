1906c00c3bd011a375fdc403ee99f266fb96cb9b
maitracle <maitracle@gmail.com>
2020-04-26 03:35:24 +0900

    add: 정식 오픈 전에 selso page에 접근했을 경우 보여줄 skeleton 컴포넌트를 추가한다.

---
+++ src/modules/SelsoList/SelsoListSkeleton.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Modal } from 'src/components/Modal/Modal';
+import SelsoItemCard from 'src/modules/SelsoList/SelsoItemCard';
+
+
+const skeletonBoySelsoItem = {
+  nickname: '기다리는 철이',
+  profile: {
+    age: 22,
+  },
+  tags: '#우리 #만나면 #뭐하고 #놀까? #멋진 #사람을 #만나고 #싶어!',
+  oneSentence: '우리 만날날을 기다리고 있어요. 기대해주세요!',
+};
+
+const skeletonGirlSelsoItem = {
+  nickname: '기다리는 순이',
+  profile: {
+    age: 22,
+  },
+  tags: '#우리 #만나면 #뭐하고 #놀까? #멋진 #사람을 #만나고 #싶어!',
+  oneSentence: '우리 만날날을 기다리고 있어요. 기대해주세요!',
+};
+
+export const SelsoListSkeleton = ({ openDate, history }) => {
+  return (
+    <>
+      <div>
+        <SelsoItemCard selsoItem={skeletonBoySelsoItem} />
+        <SelsoItemCard selsoItem={skeletonGirlSelsoItem} />
+        <SelsoItemCard selsoItem={skeletonBoySelsoItem} />
+        <SelsoItemCard selsoItem={skeletonGirlSelsoItem} />
+        <SelsoItemCard selsoItem={skeletonBoySelsoItem} />
+        <SelsoItemCard selsoItem={skeletonGirlSelsoItem} />
+        <SelsoItemCard selsoItem={skeletonBoySelsoItem} />
+        <SelsoItemCard selsoItem={skeletonGirlSelsoItem} />
+      </div>
+      <Modal
+        isOpen={true}
+        close={() => history.push('/')}
+      >
+        아직 정식 오픈 이전이에요. <br />
+        { openDate.getMonth() + 1 }월 { openDate.getDate() }일을 기다려주세요.
+      </Modal>
+    </>
+  );
+};
--- src/pages/Selso/SelsoListPage/SelsoListPage.jsx
+++ src/pages/Selso/SelsoListPage/SelsoListPage.jsx
@@ -2,14 +2,31 @@ import React from 'react';
 import SelsoList from 'src/modules/SelsoList';
 
 import styles from './SelsoListPage.module.scss';
+import { SelsoListSkeleton } from 'src/modules/SelsoList/SelsoListSkeleton';
 
-export const SelsoListPage =  ({ history }) => (
-  <div>
-    <div className={styles.titleWrapper}>
-      <h1 className={styles.title}>셀프 프로필</h1>
+
+export const SelsoListPage = ({ history }) => {
+  const selsoListOpenDate = new Date(2020, 4, 7);
+
+  const isAccessibleSelsoList = () => {
+    let today = new Date();
+
+    return today > selsoListOpenDate;
+  };
+
+  return (
+    <div>
+      <div className={styles.titleWrapper}>
+        <h1 className={styles.title}>셀프 프로필</h1>
+      </div>
+      <div className={styles.listWrapper}>
+        {
+          isAccessibleSelsoList() ?
+            <SelsoList history={history} />
+            :
+            <SelsoListSkeleton openDate={selsoListOpenDate} history={history} />
+        }
+      </div>
     </div>
-    <div className={styles.listWrapper}>
-      <SelsoList history={history} />
-    </div>
-  </div>
-);
+  );
+};
