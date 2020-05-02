a6bc8801abf56742887537d3d2ec830bd46363cb
maitracle <maitracle@gmail.com>
2020-05-02 18:07:58 +0900

    change: main page에 open date timer를 추가한다.

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from './MainPage.module.scss';
+import { yeungnamSelsoListOpenDate } from 'src/constants/dates';
+
 
 export default () => {
+  const [timerID, setTimerId] = useState(null);
+  const [leftTime, setLeftTime] = useState('D - ');
+  const [isOpenSelsoList, setIsOpenSelsoList] = useState(false);
+
+  const timer = () => setInterval(() => {
+    const now = new Date();
+    let distance = yeungnamSelsoListOpenDate.getTime() - now.getTime();
+
+    if (distance < 0) {
+      setIsOpenSelsoList(true);
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    const str = `D - ${days}일 ${hours}시간 ${minutes}분 ${seconds}초`;
+    setLeftTime(str);
+  }, 1000);
+
+  useEffect(() => {
+    // set timer
+    setTimerId(timer());
+  }, []);
+
+  useEffect(() => {
+    // clear timer
+    if (isOpenSelsoList) {
+      clearInterval(timerID);
+    }
+  }, [isOpenSelsoList]);
+
   return (
     <div className={styles.pageWrapper}>
       <div className={styles.ctaSection}>
@@ -15,7 +50,12 @@ export default () => {
           셀소 리스트 오픈날까지
           </span>
           <div className={styles.countdown}>
-            D - countdown
+            {
+              isOpenSelsoList ?
+                '셀소 오픈!'
+                :
+                leftTime
+            }
           </div>
         </div>
         <button className={styles.ctaButton}><span className={styles.buttonText}>간편하게 회원가입 하기</span></button>
