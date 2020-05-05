087b9f0f9af0b706bc7bfcc009a90b42d544e6a7
10000001a <pgdn06288888@gmail.com>
2020-05-05 18:50:22 +0900

    ADD: Add getChatLink error modal.

--- src/pages/Selso/SelsoDetailPage/SelsoDetailPage.jsx
+++ src/pages/Selso/SelsoDetailPage/SelsoDetailPage.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { inject, observer } from 'mobx-react';
 
 import Card from 'src/modules/SelsoDetail/Card';
 import styles from './SelsoDetailPage.module.scss';
+import Modal from 'src/components/Modal';
 
 
 export const SelsoDetailPage = inject('selsoListStore')(observer(({ selsoListStore }) => {
+  const [isErrorModalOpen, setIsErrorModalOpen] = useState(false);
+  const [getOpenKakaoLinkErrorMessage, setGetOpenKakaoLinkErrorMessage] = useState('');
+  
   useEffect(() => {
     if (selsoListStore.choosedSelso) {
       // choosed selso 값이 없을 경우 fetchSelsoDetail function이 undefined를 반환하여 callback을 사용할 수 없다.
@@ -24,6 +28,14 @@ export const SelsoDetailPage = inject('selsoListStore')(observer(({ selsoListSto
         if (res.status === 200) {
           window.open(res.chatLink);
         }
+        else {
+          if (res.status === 404) {
+            setGetOpenKakaoLinkErrorMessage('채팅방이 삭제되었습니다.');
+          } else {
+            setGetOpenKakaoLinkErrorMessage('잠시 후 다시 시도해주세요.');
+          }
+          setIsErrorModalOpen(true);
+        }    
       });
   };
 
@@ -34,6 +46,12 @@ export const SelsoDetailPage = inject('selsoListStore')(observer(({ selsoListSto
       <div className={styles.buttonWrapper}>
         <button onClick={getOpenKakaoLink} className={styles.kakaoButton}>오픈 카카오로 연락하기</button>
       </div>
+      <Modal
+          isOpen={isErrorModalOpen}
+          close={()=>setIsErrorModalOpen(false)}
+        >
+          <p>{getOpenKakaoLinkErrorMessage}</p>
+        </Modal>
     </div>
   );
 }));
